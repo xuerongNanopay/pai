92fc58b363a844059c4f1b81cd5b4a5fa453ac06
xuerongNanopay <xuerong.aws.ca@gmail.com>
2026-05-20 18:09:28 -0400

    add responses api folder

--- crates/pai_openai/src/responses/mod.rs
+++ crates/pai_openai/src/responses/mod.rs
@@ -1,8 +1,10 @@
-use serde::{Serialize};
+use serde::{Deserialize, Serialize};
 
 
-#[derive(Debug, Serialize)]
-
-struct InternalResponsesRequest {
-
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ResponsesRequest {
+    /// Model ID used to generate the response, like gpt-4o or o3.
+    pub model: String,
+    // /// A list of one or many input items to the model, containing different content types.
+    // pub input: Vec<InputItem>
 }
